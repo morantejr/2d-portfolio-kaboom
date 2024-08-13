fa65bbba5529cd22b9630d6efc876d616f5a3260
Joseph Morante <35009526+morantejr@users.noreply.github.com>
2024-08-12 20:36:27 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,20 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+  pc: `This is my PC. I work mostly in SQL and Python these days.
+    I've made a couple of games in that language. I also like Excel and R. Anyway regardless of the language, I just like to analyze data.
     Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
+  "msba-degree": `This is my MSBA degree from Cornell. I hung it on the wall because I'm proud of it. Although it was a very business-oriented degree, it also provided me with a foundation in machine learning.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch Youtube.`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+   <a href="https://www.youtube.com/@GameTheory" target="_blank">Game Theory</a>, <a href="https://www.youtube.com/@WatchMojo" target="_blank">WatchMojo</a>,
+  <a href="https://www.youtube.com/@comicstorian" target="_blank">Comicstorian</a> (sometimes) and <a href="https://www.youtube.com/@CoryxKenshin" target="_blank">CoryxKenshin</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/morantejr/Resume/blob/main/Joseph%20Morante%20Jr%20Resume.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  Contact me at morantejoseph1@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript. I fork this from jslegenddev.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming and non-fiction books on my shelves. There is even one in Spanish (I'm learning Spanish (again)).
+  I probably only read 10 of them. Who else compulsively buys books without ever finishing them?`,
+  exit: `If you want to exit Joe's portfolio, just close the tab.`,
 };
